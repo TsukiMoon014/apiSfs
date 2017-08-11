3f82d6bad6e358eea6905ad2c648ebee53e6d342
jessyyk <jessy.andriatsima@yellowkorner.fr>
2017-08-11 16:52:29 +0200

    clean

--- index.php
+++ index.php
@@ -4,9 +4,6 @@ namespace apiSfs;
 
 use apiSfs\app\Autoloader;
 use apiSfs\core\ConfigHandler\Config;
-use apiSfs\core\Database\Connection;
-use apiSfs\src\Colisweb\ColiswebHandler;
-use apiSfs\src\Package\PackageModel;
 use apiSfs\src\Route\RouteHandler;
 
 require_once __DIR__.'/app/Autoloader.php';
@@ -18,14 +15,5 @@ require_once 'resources/Maxmind/geoipregionvars.php';
 Autoloader::register();
 Config::loadConfig();
 
-//$routeHandler = new RouteHandler();
-//$routeHandler
-//    ->loadRoutes()
-//    ->runRouter()
-//;
-
-//$packageModel = new PackageModel(Connection::getConnection());
-$coliswebModel = new ColiswebHandler();
-dump(COLISWEB['development']['base_url']);
-dump($coliswebModel->getCarrierTiming('A90', '75003', '3662657208344'));
-//$coliswebModel->getCarrierTiming('A90', '75003', '3662657208344');
+$routeHandler = new RouteHandler();
+$routeHandler->loadRoutes()->runRouter();
